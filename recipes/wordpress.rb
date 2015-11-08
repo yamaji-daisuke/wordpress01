c9c68ab51f40d9c565eb33c604194b59675698d9
chef <chef@ChefWs.takara.org>
2015-11-08 14:52:49 +0900

    edit

--- recipes/wordpress.rb
+++ recipes/wordpress.rb
@@ -17,6 +17,7 @@ end
 script "install_wordpress" do
   interpreter "bash"
   user        "root"
+  cwd         "/root"
   code <<-EOL
     tar xzvf latest-ja.tar.gz
     mv wordpress /usr/share/nginx/html 
