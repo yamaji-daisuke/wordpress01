0f3ca4dbd08d096f178d766ee16f1426a6c13a22
高良真穂 <maho@imac.local>
2015-11-08 14:27:06 +0900

    devide default.rb

---
+++ recipes/wordpress.rb
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+# Cookbook Name:: wordpress01
+# Recipe:: default
+#
+# Copyright 2015, Maho Takara
+#
+
+remote_file '/root/latest-ja.tar.gz' do
+  source 'http://ja.wordpress.org/latest-ja.tar.gz'
+  owner 'root'
+  group 'root'
+  mode '0444'
+  action :create
+end
+
+script "install_wordpress" do
+  interpreter "bash"
+  user        "root"
+  code <<-EOL
+    tar xzvf latest-ja.tar.gz
+    mv wordpress /usr/share/nginx/html 
+  EOL
+  action :run
+end
+
+
+db_name       = node["mysql"]["db_name"]
+user_name     = node["mysql"]["user"]["name"]
+user_password = node["mysql"]["user"]["password"]
+
+template "/usr/share/nginx/html/wp-config.php" do
+  owner "root"
+  group "root"
+  mode 0644
+  source "wp-config.php.erb"
+  variables({
+    :db_name => db_name,
+    :username => user_name,
+    :password => user_password,
+  })
+  action :create
+end
+
+
+
+
